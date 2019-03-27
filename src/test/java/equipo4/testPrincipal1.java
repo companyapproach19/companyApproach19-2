fa0aaeefe22d94afc40759cf21d35945f479637d
piuland <piuland@users.noreply.github.com>
2019-03-27 19:39:16 +0100

    Add files via upload
    He intentado crear la estructura del master con las carpetas en que creo que añadiremos ficheros en el futuro pero puede que falte alguna, por ejemplo la de documentación, aunque espero que eso no sea complicado de modificar.

---
+++ src/test/java/equipo4/testPrincipal1.java
@@ -0,0 +1,44 @@
+package equipo4;
+
+
+import static org.junit.Assert.*;
+import java.util.Date;
+
+//import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Test;
+
+import equipo4.model.Lote;
+import equipo4.model.Pilsner;
+import equipo4.model.Principal;
+import equipo4.model.Stout;
+
+public class testPrincipal1 {
+	@Test
+	public void test1() throws InterruptedException {
+		Lote lote= new Lote(new Pilsner(), new Date());
+		int result = Principal.moler(lote);
+		assertEquals(result, 0);
+	}
+	@Test
+	public void test2() throws InterruptedException {
+		int result = Principal.cocinar(new Lote(new Pilsner(), new Date()));
+		assertEquals(result, 0);
+	}
+	@Test
+	public void test3() throws InterruptedException {
+		Lote lote = new Lote(new Stout(), new Date());
+		int result = Principal.cocinar(lote);
+		assertEquals(result, 0);
+	}
+	public void test4() throws InterruptedException {
+		Lote lote= new Lote(new Stout(), new Date());
+		int result = Principal.moler(lote);
+		assertEquals(result, 0);
+		
+		
+	}
+	
+	
+	
+}
