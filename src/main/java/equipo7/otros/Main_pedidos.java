20558daa072728c77144c098d8306b13274afc88
Teresa_GCG <t.gomez-carpinterog@alumnos.upm.es>
2019-03-29 09:08:05 +0100

    Clase comentada

--- src/main/java/equipo7/otros/Main_pedidos.java
+++ src/main/java/equipo7/otros/Main_pedidos.java
@@ -2,28 +2,32 @@ package equipo7.otros;
 import equipo7.model.OrdenTrazabilidad;
 public class Main_pedidos {
 	   public static OrdenTrazabilidad OrdenTrazabilidad; 
-	   public static int codorigen;//
-	   public static int  coddestino;//  	
+	   public static int codorigen; //Código del tipo de Actor que crea la petición
+	   public static int  coddestino; //Código del tipo de Actor que recibe la petición  	
 	   
+	   //Crea un objeto OrdenTrazabilidad con los datos del json
 	   public Main_pedidos(String pedido) {
 		   DescodificadorJson este=new DescodificadorJson();
 		   OrdenTrazabilidad=este.DescodificadorJson(pedido);
 	   }
 	  
+	   //Se crearan los objetos de las ordenes de los actores de origen y destino del pedido
+	   //Crea el objeto del actor de origen y este creara el de su destinatario
+	   //Devuelve el objeto de la orden del actor de origen
 	   public Orden crear_pedido () { 
 		  sacarCodigoOrigenDestino();
 		  Orden devolver = new Orden(OrdenTrazabilidad);
 			switch(codorigen) {
-			case 2:
+			case 1:
 				devolver = new CooperativaOrdenes(OrdenTrazabilidad);
 				break;
-			case 3:
+			case 2:
 				devolver = new FabricaOrdenes(OrdenTrazabilidad);
 				break;
-			case 4:
+			case 3:
 				devolver = new RetailerOrdenes(OrdenTrazabilidad);
 				break;
-			case 5:
+			case 4:
 				devolver = new TiendaOrdenes(OrdenTrazabilidad);
 				break;
 			}
@@ -31,8 +35,9 @@ public class Main_pedidos {
 			return devolver;
 	}	    
 	
+	//El sistema verifica que el pedido sea valido
+	//De tal manera que un pedido de Fábrica a Agricultor no se puede dar
 	public static boolean verificar_pedido() {
-		//System.out.println("---->El sistema verifica que el pedido sea correcto" );
 		boolean valido=false; 
 		sacarCodigoOrigenDestino();
 		if((codorigen-coddestino)==1) {valido=true;} 
@@ -40,7 +45,6 @@ public class Main_pedidos {
 	}
 	
 	public static void sacarCodigoOrigenDestino() {
-		
 		codorigen = OrdenTrazabilidad.getActorOrigen().getTipoActor();
 		coddestino = OrdenTrazabilidad.getActorDestino().getTipoActor();
 	} 
