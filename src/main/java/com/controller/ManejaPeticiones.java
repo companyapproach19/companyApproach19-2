d024c3dd7a92cff02bd98b026f883d2d627a820a
David Munuera Mazarro <david.mmazarro@alumnos.upm.es>
2019-04-01 11:14:49 +0200

    Generación de IDs aleatorios únicos para los pedidos

--- src/main/java/com/controller/ManejaPeticiones.java
+++ src/main/java/com/controller/ManejaPeticiones.java
@@ -1,6 +1,6 @@
 package com.controller;
 
-import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Scope;
@@ -14,9 +14,7 @@ import equipo6.otros.BlockchainServices;
 import equipo7.otros.CodificadorJSON;
 import equipo7.otros.Main_pedidos;
 import equipo7.otros.Orden;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 @Controller
 @SpringBootApplication
 public class ManejaPeticiones {
@@ -32,9 +30,13 @@ public class ManejaPeticiones {
 		Main_pedidos pedido = new Main_pedidos(json);
 		//TODO: configurar identificador
 		if(pedido.verificar_pedido()) {
-			int id;
-			while (id == 0) {
-
+			int id = 0;
+			boolean yaExiste = true;
+			while (id == 0 && yaExiste) {
+				// Obtiene un numero aleatorio entre 1 y 999999,
+				// que sera el ID del pedido a la hora de crearse
+				id = ThreadLocalRandom.current().nextInt(1, 1000000);
+				yaExiste = equipo5.dao.metodosCompany.existeIdOrdenTrazabilidad(id);
 			}
 			pedido.OrdenTrazabilidad.setId(id);
 			
@@ -47,13 +49,40 @@ public class ManejaPeticiones {
 		}
 		else return "ERROR";
 	}
+
+	//PARA EQUIPO 2: VISTAS
+	@Scope("request")
+	@RequestMapping("/creaPedido")
+	@ResponseBody
+	//Devuelve y recibe un json
+	public String creaOrden(
+			@RequestParam(name="json", required=true) String json) {
+
+		Main_pedidos pedido = new Main_pedidos(json);
+		//TODO: configurar identificador
+		if(pedido.verificar_pedido()) {
+			int id;
+			while (id == 0) {
+
+			}
+			pedido.OrdenTrazabilidad.setId(id);
+
+			//NECESARIO PARA TRAZABILIDAD:
+			//La orden se guardara en la base de datos
+			BlockchainServices bloque = new BlockchainServices();
+			bloque.guardarOrden(pedido.OrdenTrazabilidad);
+
+			return CodificadorJSON.crearJSON(pedido.OrdenTrazabilidad);
+		}
+		else return "ERROR";
+	}
 	
 	//PARA EQUIPO 2: VISTAS
 	@Scope("request")
 	@RequestMapping("/aceptarPedido")
 	@ResponseBody
 	public String aceptarPedido(
-			@RequestParam(name="json", required=true) String json) {
+			@RequestParam(name="id", required=true) int id) {
 		
 		Main_pedidos pedido = new Main_pedidos(json);
 		Orden origen = pedido.crear_pedido();
@@ -74,7 +103,7 @@ public class ManejaPeticiones {
 	@RequestMapping("/listoPedido")
 	@ResponseBody
 	public String listoPedido(
-			@RequestParam(name="json", required=true) String json) {
+			@RequestParam(name="id", required=true) int id) {
 		
 		Main_pedidos pedido = new Main_pedidos(json);
 		//Hay que comparar los identificadores de los ordentrazabilidad
@@ -98,7 +127,7 @@ public class ManejaPeticiones {
 	@RequestMapping("/recogidoPedido")
 	@ResponseBody
 	public String recogidoPedido(
-			@RequestParam(name="json", required=true) String json) {
+			@RequestParam(name="id", required=true) int id) {
 		
 		Main_pedidos pedido = new Main_pedidos(json);
 		//Hay que compara los identificadores de los ordentrazabilidad
@@ -121,7 +150,7 @@ public class ManejaPeticiones {
 	@RequestMapping("/entregadoPedido")
 	@ResponseBody
 	public String entregadoPedido(
-			@RequestParam(name="json", required=true) String json) {
+			@RequestParam(name="id", required=true) int id) {
 		
 		Main_pedidos pedido = new Main_pedidos(json);
 		//Hay que compara los identificadores de los ordentrazabilidad
