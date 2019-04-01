f239f51286809d89bfb5675db065b9db506bc922
luisbravocollado <luis.bravo.collado@alumnos.upm.es>
2019-04-01 11:53:21 +0200

    este

---
+++ src/test/java/equipo7/Orden.java
@@ -0,0 +1,14 @@
+package equipo7;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Orden {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/equipo7/OrdenTrazabilidadTest.java
@@ -0,0 +1,10 @@
+package equipo7;
+
+public class OrdenTrazabilidadTest {
+
+	
+	//probamos actor invalido
+	
+	
+	//probamos actor valido
+}
