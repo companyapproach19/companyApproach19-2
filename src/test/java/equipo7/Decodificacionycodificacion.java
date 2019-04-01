7e0734c3fb4dd96287768e88401744b16b376a59
luisbravocollado <luis.bravo.collado@alumnos.upm.es>
2019-04-01 11:52:35 +0200

    este

--- src/test/java/equipo7/Decodificacionycodificacion.java
+++ src/test/java/equipo7/Decodificacionycodificacion.java
@@ -23,19 +23,21 @@ public class Decodificacionycodificacion {
 	Actor receptor =new Actor();
 	Productos m=new Productos(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 	OrdenTrazabilidad ab=new OrdenTrazabilidad(2,"Fragil", emisor,receptor,m);
+	DescodificadorJson desc = new DescodificadorJson ();
 	
 	 @Test
 	 public void test() { //arreglar
 		  OrdenTrazabilidad ab=new OrdenTrazabilidad(2,"Fragil", emisor,receptor,m); 
 		    String cod1= CodificadorJSON.crearJSON(ab); 
-		    OrdenTrazabilidad resultado = DescodificadorJson.DescodificadorJson(cod1);  
+		    
+		    OrdenTrazabilidad resultado = desc.DescodificadorJson(cod1);  
 		    assertSame(resultado,ab);
 		  }
 	 @Test
 	 public void test1() {
 			OrdenTrazabilidad ab=new OrdenTrazabilidad(2,"Fragil", emisor,receptor,m); 
 		    String cod1= CodificadorJSON.crearJSON(ab); 
-		    OrdenTrazabilidad resultado = DescodificadorJson.DescodificadorJson(cod1); 
+		    OrdenTrazabilidad resultado = desc.DescodificadorJson(cod1); 
 		    assertEquals(cod1,  CodificadorJSON.crearJSON(resultado));
 		  }
 }
