4b0b47f09de7a5d00a4f851eb07fc81baab3bfdf
luisbravocollado <luis.bravo.collado@alumnos.upm.es>
2019-04-01 11:43:22 +0200

    test

---
+++ src/test/java/equipo7/Decodificacionycodificacion.java
@@ -0,0 +1,41 @@
+package equipo7;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import equipo6.model.Actor;
+import equipo7.model.*;
+import equipo7.model.OrdenTrazabilidad.EstadoOrden;
+import equipo7.otros.CodificadorJSON;
+import equipo7.otros.DescodificadorJson; 
+
+import equipo6.model.Actor;
+import equipo7.model.OrdenTrazabilidad;
+import equipo7.model.Productos;
+import equipo7.otros.CodificadorJSON;
+import equipo7.otros.DescodificadorJson;
+
+public class Decodificacionycodificacion {
+	Actor emisor =new Actor();
+	Actor receptor =new Actor();
+	Productos m=new Productos(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+	OrdenTrazabilidad ab=new OrdenTrazabilidad(2,"Fragil", emisor,receptor,m);
+	
+	 @Test
+	 public void test() { //arreglar
+		  OrdenTrazabilidad ab=new OrdenTrazabilidad(2,"Fragil", emisor,receptor,m); 
+		    String cod1= CodificadorJSON.crearJSON(ab); 
+		    OrdenTrazabilidad resultado = DescodificadorJson.DescodificadorJson(cod1);  
+		    assertSame(resultado,ab);
+		  }
+	 @Test
+	 public void test1() {
+			OrdenTrazabilidad ab=new OrdenTrazabilidad(2,"Fragil", emisor,receptor,m); 
+		    String cod1= CodificadorJSON.crearJSON(ab); 
+		    OrdenTrazabilidad resultado = DescodificadorJson.DescodificadorJson(cod1); 
+		    assertEquals(cod1,  CodificadorJSON.crearJSON(resultado));
+		  }
+}
