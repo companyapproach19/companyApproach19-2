8b6f77cad137dc90bb82d1d3f0de8788e03c1dba
Marisol Barrientos Moreno <ms28@hotmail.es>
2019-04-03 01:48:20 +0200

    Update Sensor.java

--- src/main/java/equipo8/model/Sensor.java
+++ src/main/java/equipo8/model/Sensor.java
@@ -6,10 +6,10 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Iterator;
 
-
+//PENDIENTE PONER CONSTRUCTOR
 public class Sensor {
 	
-    private static int id;
+    	private static int id;
 	//Aquí se encuentra el .txt con el registro
   	private static BufferedReader log; 
   	
@@ -34,49 +34,7 @@ public class Sensor {
   	// Cambiar por la ruta donde esté el log que se quiera probar
   	private static String ruta="C:\\Users\\Laura Colomer\\Documents\\datosSensor.txt" ;
   	
-  	//Permite buscar la temperatura ó humedad de una determinada fecha y hora dentro del registro
-	public String buscar(String anioParam, String mesParam, String diaParam, String horaParam, String minParam, String secParam) throws IOException {
-		// log contiene todo el registro de temperaturas
-		log = new BufferedReader(new FileReader(ruta)); 
-		// Va leyendo línea por línea...
-		while ((linea = log.readLine()) != null) {
-			//Se saltan las lineas vacías
-			if(!linea.isEmpty()){
-				partes  = linea.split(" ");
-				fecha = partes[0];
-				partesFecha  = fecha.split("-");
-				anio=partesFecha [0];
-				//Si coincide toda la fecha...
-				if(anio.equals(anioParam)) {
-					mes=partesFecha [1];
-					if(mes.equals(mesParam)) {
-						dia=partesFecha [2];
-						if(dia.equals(diaParam)) {
-							hora = partes[1];
-							partesHora = hora.split(":");
-							hora=partesHora[0];
-							//Si coincide toda la hora...
-							if(hora.equals(horaParam)){
-								min=partesHora[1];
-								if(min.equals(minParam)){
-									sec=partesHora[2];
-									if(sec.equals(secParam)){
-										if(partes[2].contains("ºC")){
-											//Devuelve la temperatura "XºC"
-											return partes[2];
-										}
-									}																		
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-		log.close();
-		return "Incorrecto";
-	}
-	
+  	
 	
 	//Cambiar el tipo de retorno a Regsitro
 	public  Object registro ()throws IOException {
@@ -133,75 +91,57 @@ public class Sensor {
 		
 	}
 
-	//Futuros gettes y setters
-	public static String getTemperatura() {
-		return temperatura;
-	}
-
-	public static void setTemperatura(String temperatura) {
-		Sensor.temperatura = temperatura;
-	}
-
-
-	public static String getAnio() {
-		return anio;
-	}
-
-	public static void setAnio(String anio) {
-		Sensor.anio = anio;
-	}
-
-
-	public static String getMes() {
-		return mes;
-	}
-
-	public static void setMes(String mes) {
-		Sensor.mes = mes;
-	}
-
-
-	public static String getDia() {
-		return dia;
-	}
-
-	public static void setDia(String dia) {
-		Sensor.dia = dia;
-	}
-
-
-	public static String getHora() {
-		return hora;
-	}
-
-	public static void setHora(String hora) {
-		Sensor.hora = hora;
-	}
-
-
-	public static String getMin() {
-		return min;
-	}
-
-	public static void setMin(String min) {
-		Sensor.min = min;
-	}
-
-
-	public static String getSec() {
-		return sec;
-	}
-
-	public static void setSec(String sec) {
-		Sensor.sec = sec;
-	}
 
 	public static void setID (int id){
-    Sensor.id = id;
- }
- public static int getID (){
-  return id;
- }
+    		Sensor.id = id;
+ 	}
+ 	public static int getID (){
+  		return id;
+ 	}
+	
+	//Permite buscar la temperatura ó humedad de una determinada fecha y hora dentro del registro
+	public String buscar(String anioParam, String mesParam, String diaParam, String horaParam, String minParam, String secParam) throws IOException {
+		// log contiene todo el registro de temperaturas
+		log = new BufferedReader(new FileReader(ruta)); 
+		// Va leyendo línea por línea...
+		while ((linea = log.readLine()) != null) {
+			//Se saltan las lineas vacías
+			if(!linea.isEmpty()){
+				partes  = linea.split(" ");
+				fecha = partes[0];
+				partesFecha  = fecha.split("-");
+				anio=partesFecha [0];
+				//Si coincide toda la fecha...
+				if(anio.equals(anioParam)) {
+					mes=partesFecha [1];
+					if(mes.equals(mesParam)) {
+						dia=partesFecha [2];
+						if(dia.equals(diaParam)) {
+							hora = partes[1];
+							partesHora = hora.split(":");
+							hora=partesHora[0];
+							//Si coincide toda la hora...
+							if(hora.equals(horaParam)){
+								min=partesHora[1];
+								if(min.equals(minParam)){
+									sec=partesHora[2];
+									if(sec.equals(secParam)){
+										if(partes[2].contains("ºC")){
+											//Devuelve la temperatura "XºC"
+											return partes[2];
+										}
+									}																		
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		log.close();
+		return "Incorrecto";
+	}
+	
 	
 	//clase auxiliar que vamos a usar para facilitar 
  private class Fecha {
