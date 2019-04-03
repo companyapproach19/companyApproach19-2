9dc9d590082da2bb911116c474812e6056e4ad24
Marisol Barrientos Moreno <ms28@hotmail.es>
2019-04-04 01:12:59 +0200

    Update PruebasSensorYRegistro.java

--- src/main/java/equipo8/model/PruebasSensorYRegistro.java
+++ src/main/java/equipo8/model/PruebasSensorYRegistro.java
@@ -6,11 +6,11 @@ public class PruebasSensorYRegistro {
 		Sensor sensor = new Sensor(54);
 		Registro reg1,reg2,reg3;
 		try {
-			reg1 = sensor.crearRegistro(609,990,"/home/marisol/Desktop/Pruebas/2019-02-03|11:03:23.txt");
+			reg1 = sensor.crearRegistro(609,990,"/home/marisol/Desktop/Pruebas/2019-04-03|11:03:23.txt");
 			System.out.println(reg1.toString());
-			reg2 = sensor.crearRegistro(8869,75,"/home/marisol/Desktop/Pruebas/2019-02-05|11:03:23.txt");
+			reg2 = sensor.crearRegistro(8869,75,"/home/marisol/Desktop/Pruebas/2019-04-05|11:03:23.txt");
 			System.out.println(reg2.toString());
-			reg3 = sensor.crearRegistro(1008,7565,"/home/marisol/Desktop/Pruebas/2019-02-04|11:03:23.txt");
+			reg3 = sensor.crearRegistro(1008,7565,"/home/marisol/Desktop/Pruebas/2019-04-04|11:03:23.txt");
 			System.out.println(reg3.toString());
 		} catch (Exception e) {
 			e.printStackTrace();
