b56a543bf685a48f6de69d762ad4e9cebc5572e4
gonzalo194753 <satrusteguigonzalo@gmail.com>
2019-04-05 01:10:17 +0200

    Añadido el superBloque junto con los metodos de guardarRespuestaPedido que enlaza los distintos blockchain en una sola cadena y get_trazabilidad que te proporciona un json con toda la trazabilidad del pedido

--- src/main/java/com/controller/StockController.java
+++ src/main/java/com/controller/StockController.java
@@ -48,16 +48,83 @@ public class StockController {
 		json_resp = new JsonObject();
 		
 		
-		actor = metodosCompany.getCadenaActores().get_actor_by_id(idActor);
+		//actor = metodosCompany.getCadenaActores().get_actor_by_id(idActor);
+		actor=new Actor(idActor, "Agricultor", "asdasd", "rmj@g.cm", 0, "41.5N 2.0W","Agricultor A", "c/mevoyamorir", "1234567C");
 		
 		json_resp.addProperty("nomUsuario", actor.getNombreUsuario());
 		json_resp.addProperty("email", actor.getEmail());
 		json_resp.addProperty("tipoActor", actor.getTipoActor());
-		json_resp.add("stock", new JsonObject());
+		JsonObject stock = new JsonObject();
+		//stock.addProperty("malta_palida", metodosCompany.extraerStockMateriaPrima(actor, MateriaPrima materiaPrima));
+		stock.addProperty("malta_palida", 10);
+		stock.addProperty("malta_tostada", 11);
+		stock.addProperty("malta_negra", 12);
+		stock.addProperty("malta_crystal", 13);
+		stock.addProperty("malta_chocolate", 14);
+		stock.addProperty("malta_caramelo",15);
+		stock.addProperty("malta_pilsner", 16);
+		stock.addProperty("malta_munich", 17);
+		stock.addProperty("lupulo_perle", 18);
+		stock.addProperty("lupulo_tettnager", 19);
+		stock.addProperty("lupulo_centennial", 20);
+		stock.addProperty("levadura_ale", 21);
+		stock.addProperty("levadura_lagger", 22);
+		stock.addProperty("lotes_pilsner", 0);
+		stock.addProperty("lotes_stout", 0);
+		
+		
+		
+		
+		json_resp.add("stock", stock);
 		//obtener el stock asociado a un actor consultando a la base de datos
 		//metodosCompani.dameStockActor(string idActor)
 		
 		return json_resp.toString();
+		
+		
+		/*
+		 {
+
+  "nomUsuario":"nombre del actor" ,
+
+  "email":"email asociado" ,
+
+  "tipoActor":"tipo asociado" ,
+
+  "stock" {
+
+   "malta_palida": "la cantidad que tenga",
+
+   "malta_tostada": "la cantidad que tenga",
+
+   "malta_negra": "la cantidad que tenga",
+
+   "malta_crystal": "la cantidad que tenga",
+
+   "malta_chocolate": "la cantidad que tenga",
+
+   "malta_caramelo": "la cantidad que tenga",
+
+   "malta_pilsner": "la cantidad que tenga",
+
+   "malta_munich": "la cantidad que tenga",
+
+   "lupulo_perle": "la cantidad que tenga",
+
+   "lupulo_tettnager": "la cantidad que tenga",
+
+   "lupulo_centennial": "la cantidad que tenga",
+
+   "levadura_ale": "la cantidad que tenga",
+
+   "levadura_lagger": "la cantidad que tenga",    
+   
+   "lotes_pilsner":"la cantidad de lotes que tenga",
+
+   "lotes_stout":"la cantidad de lotes que tenga",   }
+
+}
+		 */
 		
 		
 	}
