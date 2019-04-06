51c974b3b0f14f64d43e86b5f01c0622afada241
JoseVivesPazos <48154019+JoseVivesPazos@users.noreply.github.com>
2019-04-06 23:26:40 +0200

    ULTIMISIMA VERSION

--- src/main/resources/public/fabrica2.js
+++ src/main/resources/public/fabrica2.js
@@ -11,23 +11,35 @@ $(document).ready(function(){
         $("#popup1").hide();
         return false;
     });
+    $("#hide2").on('click', function() {
+        $("#popup2").hide();
+        return false;
+    });
+    $("#hide3").on('click', function() {
+        $("#popup3").hide();
+        return false;
+    });
+    $("#hide4").on('click', function() {
+        $("#popup4").hide();
+        return false;
+    });
     $("#bt1").on('click', function() {
-  pedirStock("1");
+	pedirStock("1");
         $("#popup1").show();
         return false;
     });
     $("#bt2").on('click', function() {
-  pedirStock("2");
+	pedirStock("2");
         $("#popup2").show();
         return false;
     });
     $("#bt3").on('click', function() {
-  pedirStock("3");
+	pedirStock("3");
         $("#popup3").show();
         return false;
     });
     $("#bt4").on('click', function() {
-  pedirStock("4");
+	pedirStock("4");
         $("#popup4").show();
         return false;
     });
@@ -38,60 +50,60 @@ $(document).ready(function(){
 function pedirStock(actor) {
 
     var request = $.ajax({
-  
-  // la URL para la petición del stock
-  //TODO: Ponerla bien
-  url : '/dameStockActor',
-  
-  // la información a enviar (parámetros que se le pasan a la url)
-  // (también es posible utilizar una cadena de datos)
-  data : "id=" + actor,
-  
-  // especifica si será una petición POST o GET
-  type : 'POST',
-  
-  // el tipo de información que se espera de respuesta
-  dataType : 'json',
-  
+	
+	// la URL para la petición del stock
+	//TODO: Ponerla bien
+	url : '/dameStockActor',
+	
+	// la información a enviar (parámetros que se le pasan a la url)
+	// (también es posible utilizar una cadena de datos)
+	data : "id=" + actor,
+	
+	// especifica si será una petición POST o GET
+	type : 'POST',
+	
+	// el tipo de información que se espera de respuesta
+	dataType : 'json',
+	
     });
     // código a ejecutar si la petición es satisfactoria;
     // la respuesta es pasada como argumento a la función
     request.done(function(data){
-  
-  rellenaPopup(JSON.parse(data), actor);
-  
+	
+	rellenaPopup(JSON.parse(data), actor);
+	
     });
     request.fail(function(data) {
-  // Escribe en el popup "Petición al servidor fallida"
-  // De momento, si la petición falla lee el JSON auxiliar que tenemos
-  $("popup" + actor).text("Petición al servidor fallida. Se utilizarán datos locales");
-  
-  switch(actor) {
-  case "1": 
-      rellenaPopup(JSON.parse(json_aux1), actor);
-      break;
-  case "3": 
-      rellenaPopup(JSON.parse(json_aux2), actor);
-      break;
-  case "2": 
-      rellenaPopup(JSON.parse(json_aux3), actor);
-      break;
-  case "4": 
-      rellenaPopup(JSON.parse(json_aux4), actor);
-      break;
-  default:
-      break;
-  }
-  
+	// Escribe en el popup "Petición al servidor fallida"
+	// De momento, si la petición falla lee el JSON auxiliar que tenemos
+	$("popup" + actor).text("Petición al servidor fallida. Se utilizarán datos locales");
+	
+	switch(actor) {
+	case "1": 
+	    rellenaPopup(JSON.parse(json_aux1), actor);
+	    break;
+	case "2": 
+	    rellenaPopup(JSON.parse(json_aux2), actor);
+	    break;
+	case "3": 
+	    rellenaPopup(JSON.parse(json_aux3), actor);
+	    break;
+	case "4": 
+	    rellenaPopup(JSON.parse(json_aux4), actor);
+	    break;
+	default:
+	    break;
+	}
+	
     });
 }
 function rellenaPopup(stock,actor) {
     
     if (stock == null) {
-  // En este caso ni el servidor ha devuelto nada ni tenemos JSON local
-  // Escribir en el popup "Respuesta del servidor errónea" y terminar
-  $("popup" + actor).append("<br><br>Respuesta del servidor o datos locales erróneos. No hay stock para mostrar");
-  return;
+	// En este caso ni el servidor ha devuelto nada ni tenemos JSON local
+	// Escribir en el popup "Respuesta del servidor errónea" y terminar
+	$("popup" + actor).append("<br><br>Respuesta del servidor o datos locales erróneos. No hay stock para mostrar");
+	return;
     }
     
     // Leer info del JSON y ponerla en el popup correspondiente
@@ -121,10 +133,57 @@ function rellenaPopup(stock,actor) {
     }   
 }
 
-var json_aux1 = '{"nomUsuario":"Agricultor1","tipoActor":"Agricultor","email":"agricultor1@email.com","stock":{ "malta_palida":"2000","malta_palida":"2343","malta_tostada":"234252","malta_negra":"74564","malta_crystal":"09340234","malta_chocolate":"324","malta_caramelo":"90428042","malta_pilsner":"23424","malta_munich":"54353","lupulo_perle":"4242","lupulo_tettnager":"4242342","lupulo_centennial":"34242","levadura_ale": "34243","levadura_lagger": "84092","lotes_pilsner":"","lotes_stout":""}}';
+var json_aux1 = '{"nomUsuario":"Nelson el Agricultor","tipoActor":"Agricultor","email":"terratenienteNelson@email.com","stock":{ "malta_palida":"2000","malta_palida":"2343","malta_tostada":"234252","malta_negra":"74564","malta_crystal":"09340234","malta_chocolate":"324","malta_caramelo":"90428042","malta_pilsner":"23424","malta_munich":"54353","lupulo_perle":"4242","lupulo_tettnager":"4242342","lupulo_centennial":"34242","levadura_ale": "34243","levadura_lagger": "84092","lotes_pilsner":"","lotes_stout":""}}';
 
-var json_aux2 = '{"nomUsuario":"Cooperativa1","tipoActor":"Cooperativa","email":"cooperativa1@email.com","stock":{ "malta_palida":"2000", "malta_palida": "2343","malta_tostada": "234252","malta_negra": "74564","malta_crystal": "09340234","malta_chocolate": "324","malta_caramelo": "90428042","malta_pilsner": "23424","malta_munich": "54353","lupulo_perle": "4242","lupulo_tettnager": "4242342", "lupulo_centennial": "34242","levadura_ale": "34243","levadura_lagger": "84092","lotes_pilsner":"","lotes_stout":""}}';
+var json_aux2 = '{"nomUsuario":"Nelson el Cooperativista","tipoActor":"Cooperativa","email":"cooperativasNelsonSL@email.com","stock":{ "malta_palida":"2000", "malta_palida": "2343","malta_tostada": "234252","malta_negra": "74564","malta_crystal": "09340234","malta_chocolate": "324","malta_caramelo": "90428042","malta_pilsner": "23424","malta_munich": "54353","lupulo_perle": "4242","lupulo_tettnager": "4242342", "lupulo_centennial": "34242","levadura_ale": "34243","levadura_lagger": "84092","lotes_pilsner":"","lotes_stout":""}}';
 
-var json_aux3 = '{"nomUsuario":"Fábrica1","tipoActor":"Fabrica","email":"fabrica1@email.com","stock":{ "malta_palida":"2000","malta_palida": "2343","malta_tostada": "234252","malta_negra": "74564","malta_crystal": "09340234","malta_chocolate": "324","malta_caramelo": "90428042","malta_pilsner": "23424","malta_munich": "54353","lupulo_perle": "4242","lupulo_tettnager": "4242342","lupulo_centennial": "34242","levadura_ale": "34243","levadura_lagger": "84092","lotes_pilsner":"32424","lotes_stout":"23424"}}';
+var json_aux3 = '{"nomUsuario":"Nelson el Fabricante","tipoActor":"Fabrica","email":"nelsonFactory@email.com","stock":{ "malta_palida":"2000","malta_palida": "2343","malta_tostada": "234252","malta_negra": "74564","malta_crystal": "09340234","malta_chocolate": "324","malta_caramelo": "90428042","malta_pilsner": "23424","malta_munich": "54353","lupulo_perle": "4242","lupulo_tettnager": "4242342","lupulo_centennial": "34242","levadura_ale": "34243","levadura_lagger": "84092","lotes_pilsner":"32424","lotes_stout":"23424"}}';
 
-var json_aux4 = '{"nomUsuario":"Retailer1","tipoActor":"Retailer","email":"retailer1@email.com", "stock":{ "malta_palida":"","malta_palida": "","malta_tostada": "","malta_negra": "","malta_crystal": "","malta_chocolate": "","malta_caramelo": "","malta_pilsner": "","malta_munich": "","lupulo_perle": "","lupulo_tettnager": "","lupulo_centennial": "","levadura_ale": "","levadura_lagger": "","lotes_pilsner":"94208","lotes_stout":"23424"}}';
+var json_aux4 = '{"nomUsuario":"Nelson el Comerciante","tipoActor":"Retailer","email":"barCafeteriaNelson@email.com", "stock":{ "malta_palida":"","malta_palida": "","malta_tostada": "","malta_negra": "","malta_crystal": "","malta_chocolate": "","malta_caramelo": "","malta_pilsner": "","malta_munich": "","lupulo_perle": "","lupulo_tettnager": "","lupulo_centennial": "","levadura_ale": "","levadura_lagger": "","lotes_pilsner":"94208","lotes_stout":"23424"}}';
+
+
+
+/*******************ANIMACIONNNNNNNNNNNNNNNNNN*/////////////////////
+document.addEventListener("DOMContentLoaded", function(event) { 
+  initialize();
+});
+
+
+
+function initialize(){
+
+	var select = function(s) { return document.querySelector(s); };
+	var selectAll = function(s) { return document.querySelectorAll(s); }
+	TweenMax.set('#ad', {opacity:1});
+
+	var banner = new TimelineMax({repeat:0, delay:1, yoyo:false, paused:false});
+	/*frame1*/
+	banner
+	
+	
+	
+	.from('#bt1', 1, {alpha:0, ease:Back.easeOut})
+	.from('#linea1', 1, {scale:0})
+	
+	.from('#bt3', 1, {alpha:0, ease:Back.easeOut})
+	
+	
+	.to(linea2bis, 1, {scale:0})
+	.from('#bt2', 1, {alpha:0, ease:Back.easeOut})
+	.from('#linea3', 1, {scale:0})
+	.from('#bt4', 1, {alpha:0, ease:Back.easeOut})
+	
+	
+	
+	
+	
+	
+
+	
+	
+
+
+	
+	
+	
+}
