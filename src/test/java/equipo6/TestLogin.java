08ab4daaf42d59e90ff323cbbfcdf3e953c807d4
gonzalo194753 <satrusteguigonzalo@gmail.com>
2019-04-09 13:17:47 +0200

    Modificado el test de TestTrazabilidad y añadidas las iteraciones de StockController en TestTemplate

--- src/test/java/equipo6/TestLogin.java
+++ src/test/java/equipo6/TestLogin.java
@@ -13,42 +13,109 @@ import equipo6.objetosTemporales.BBDDTemporal;
 
 class TestLogin {
 	@Test
-	void test() {
-		testInterno();
-		testServicioWeb();
+	void test1() {
+		Actor actor_login;
+		UsuariosService us;
+		Actor respuesta;
+		
+		us = new UsuariosService();
+		actor_login = new Actor("Agricultor", "password");
+		
+		try {
+			respuesta = us.logUsuario(actor_login);
+			if(respuesta.getTipoActor() != -1) 
+			{
+				System.out.println("actor logeado");
+			}
+			else 
+			{
+				System.out.println("\nactor incorrecto");
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 	
-	void testInterno() {
-		BBDDTemporal bd = new BBDDTemporal();
-		CadenaActores cad = new CadenaActores();
-		/*
-		Actor ac=new Actor("jorge","jorge","jorge@gmail.com",new Actor(),0,0);
-		cad.addActor(ac);
-		Actor ac2=new Actor("jorge2","jorge2","jorge@gmail.com",ac,1,0);
-		cad.addActor(ac2);
-		Actor ac3=new Actor("jorge3","jorge3","jorge@gmail.com",ac,2,0);
-		cad.addActor(ac3);
-		bd.guardarCadenaActores(cad);
+	@Test
+	void test2() {
+		Actor actor_login;
+		UsuariosService us;
+		Actor respuesta;
 		
-		UsuariosService usrv = new UsuariosService();
-		usrv.init(bd);*/
+		us = new UsuariosService();
+		actor_login = new Actor("Agricultor", "contraseña_no_valida");
 		
+		try {
+			respuesta = us.logUsuario(actor_login);
+			if(respuesta.getTipoActor() != -1) 
+			{
+				System.out.println("actor logeado");
+			}
+			else 
+			{
+				System.out.println("\nactor incorrecto");
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		Actor acCorrecto=new Actor("jorge","jorge");		
-		//Actor resp=usrv.logUsuario(acCorrecto);
-		//if(resp!=null)System.out.println(resp.getNombreUsuario());
-		
-		Actor acIncorrecto=new Actor("jorge4","jorge4");		
-		//resp=usrv.logUsuario(acIncorrecto);
-		//if(resp!=null)System.out.println(resp.getNombreUsuario());
 	}
 	
-	void testServicioWeb() {
-//		URL url = new URL("http://example.com");
-//		HttpURLConnection con = (HttpURLConnection) url.openConnection();
-//		con.setRequestMethod("GET");
-//		
-//		con.getOutputStream()
+	@Test
+	void test3() {
+		Actor actor_login;
+		UsuariosService us;
+		Actor respuesta;
+		
+		us = new UsuariosService();
+		actor_login = new Actor("usuario_no_valido", "password");
+		
+		try {
+			respuesta = us.logUsuario(actor_login);
+			if(respuesta.getTipoActor() != -1) 
+			{
+				System.out.println("actor logeado");
+			}
+			else 
+			{
+				System.out.println("\nactor incorrecto");
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
+	
+	@Test
+	void test4() {
+		Actor actor_login;
+		UsuariosService us;
+		Actor respuesta;
+		
+		us = new UsuariosService();
+		actor_login = new Actor("usuario_no_valido", "contraseña_no_valida");
+
+		
+		try {
+			respuesta = us.logUsuario(actor_login);
+			if(respuesta.getTipoActor() != -1) 
+			{
+				System.out.println("actor logeado");
+			}
+			else 
+			{
+				System.out.println("\nactor incorrecto");
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
 
 }
---
+++ src/test/java/equipo6/TestStockController.java
@@ -0,0 +1,152 @@
+package equipo6;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.controller.StockController;
+
+import equipo4.model.Lote;
+import equipo4.model.MateriaPrima;
+import equipo4.model.Pilsner;
+import equipo5.dao.NotInDatabaseException;
+import equipo5.dao.metodosCompany;
+import equipo6.model.Actor;
+
+class TestStockController {
+
+	// test hecho anadiendo a mano maltaCrystal en la bbdd con el id pertinente y
+	// comentando en el constructor de materia prima esta linea:
+	// this.id=metodosCompany.idMateriaPrima();
+	@Test
+	// test de getCantidadStock cuando el actor no tiene MP
+	void test() throws ClassNotFoundException, SQLException, NotInDatabaseException {
+		Actor p = metodosCompany.extraerActor("Retailer");
+		MateriaPrima mp = new MateriaPrima("maltaCrystal");
+		double result = StockController.getCantidadStock(p, mp);
+		System.out.println(result);
+	}
+	
+	@Test
+	// test de getCantidadStock cuando el actor tiene MP, esta comentado el codigo para no petar la bbdd
+	void test1() throws ClassNotFoundException, SQLException, NotInDatabaseException {
+		Actor p = metodosCompany.extraerActor("Productor");
+		MateriaPrima mp = new MateriaPrima("maltaCrystal");
+		double result = StockController.getCantidadStock(p, mp);
+		System.out.println(result);
+	}
+	
+	@Test
+	//test de setCantidadMateriaPrima cuando el actor no tiene MP
+	void test2() throws ClassNotFoundException, SQLException, NotInDatabaseException {
+
+		Actor p=metodosCompany.extraerActor("Retailer");
+		MateriaPrima mp= new MateriaPrima("maltaCrystal");
+		
+		StockController.setCantidadMateriaPrima(p,mp, 300);
+		System.out.println(StockController.getCantidadStock(p, mp));
+
+		
+	}
+		@Test
+	//test de setCantidadMateriaPrima cuando el actor tiene MP, esta comentado el codigo para no petar la bbdd
+	void test3() throws ClassNotFoundException, SQLException, NotInDatabaseException {
+
+		Actor p=metodosCompany.extraerActor("Productor");
+		MateriaPrima mp= new MateriaPrima("maltaCrystal");
+		
+		StockController.setCantidadMateriaPrima(p,mp, 300);
+		System.out.println(StockController.getCantidadStock(p, mp));
+
+		
+	}
+
+
+	//=======================================
+	
+	
+	//estos test han pasado hardcoreando Pilsner y cambiando sentencias que estaban mal
+	//en bbdd
+	
+
+	@Test
+	// test de getStockLote para cuando el actor no tiene Lotes
+	void test4() throws ClassNotFoundException, SQLException, NotInDatabaseException {
+		Actor actor = metodosCompany.extraerActor("Productor");
+		Pilsner p = new Pilsner();
+		Lote lote = new Lote(p, new java.util.Date());
+		int cantidad = StockController.getStockLote(actor, lote);
+		System.out.println("cuando el actor no tiene lotes "+cantidad);
+
+	}
+
+	
+	
+	@Test
+	// test de getStockLote para cuando el actor si tiene Lotes
+	void test5() throws ClassNotFoundException, SQLException, NotInDatabaseException {
+		Actor actor = metodosCompany.extraerActor("Retailer");
+		Pilsner p = new Pilsner();
+		Lote lote = new Lote(p, new java.util.Date());
+		int cantidad = StockController.getStockLote(actor, lote);
+		System.out.println("cuando el actor tiene lotes "+cantidad);
+
+	}
+	
+	@Test
+	// test de setCantidadLote para cuando el actor no tiene Lotes
+	void test6() throws ClassNotFoundException, SQLException, NotInDatabaseException {
+		Actor actor = metodosCompany.extraerActor("Productor");
+		Pilsner p = new Pilsner();
+		Lote lote = new Lote(p, new java.util.Date());
+		try {
+			int cantidad = StockController.getStockLote(actor, lote);
+			System.out.println("cuando el actor no tiene lotes cantidad actual "+cantidad);
+			StockController.setCantidadLote(actor, lote);
+			System.out.println("cuando el actor no tiene lotes cantidad despues de anadir "+cantidad);
+		} catch (Throwable e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}	
+	
+	
+	@Test
+	// test de setCantidadLote para cuando el actor si tiene Lotes, no funciona 
+	//el metodo insertarLote de metodosCompany por las fechas, hay que crear una nueva instancia
+	//no vale con un casting
+	void test7() throws ClassNotFoundException, SQLException, NotInDatabaseException {
+		Actor actor = metodosCompany.extraerActor("Retailer");
+		Pilsner p = new Pilsner();
+		Lote lote = new Lote(p, new java.util.Date());
+		try {
+			int cantidad = StockController.getStockLote(actor, lote);
+			System.out.println("cuando el actor tiene lotes cantidad actual "+cantidad);
+			StockController.setCantidadLote(actor, lote);
+			cantidad = StockController.getStockLote(actor, lote);
+			System.out.println("cuando el actor tiene lotes cantidad despues de anadir "+cantidad);
+		} catch (Throwable e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}	
+	
+	@Test
+	//este test da error pero en la base de datos se modifica bien
+	//test de setCantidadMateriaPrima cuando el actor tiene MP, esta comentado el codigo para no petar la bbdd
+	void test8() throws ClassNotFoundException, SQLException, NotInDatabaseException {
+
+		Actor p=metodosCompany.extraerActor("Fabrica");
+		MateriaPrima mp= new MateriaPrima("maltaCrystal");
+		StockController.setCantidadMateriaPrima(p,mp, 1739130434);
+		System.out.println(StockController.getCantidadStock(p, mp));
+	}
+
+}
