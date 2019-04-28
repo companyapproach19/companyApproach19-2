275e3a0028b560aca618f47e502c361e33dfd848
Marisol Barrientos Moreno <ms28@hotmail.es>
2019-04-28 17:53:52 +0200

    Update TestSensorRegistro.java

--- src/test/java/equipo8/TestSensorRegistro.java
+++ src/test/java/equipo8/TestSensorRegistro.java
@@ -11,7 +11,7 @@ public class TestSensorRegistro {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
 		Sensor reg= new Sensor(2);
 		reg.crearRegistroCadaXMinutos(999, 999, "datosSensorMiercoles.txt", 1);
-		reg.pararCreacionRegistros("datosSensorMiercoles.txt");
+		//reg.pararCreacionRegistros("datosSensorMiercoles.txt");
 	}
 
 }
