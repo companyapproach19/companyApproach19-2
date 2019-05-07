5ce8e647790a5f21d31cb09f80ef90568fb08443
piuland <piuland@users.noreply.github.com>
2019-05-07 18:44:18 +0200

    corregido error

--- src/main/java/equipo4/model/Principal.java
+++ src/main/java/equipo4/model/Principal.java
@@ -54,7 +54,7 @@ public class Principal extends Thread {
 				Lote lote2 = lote1.getLote();
 				Date fechaInicial = (Date) lote2.getFecha_inicio();
 				Date fechaActual = new Date(System.currentTimeMillis());
-				int tiempo= fechaInicial.getMinutes()-fechaActual.getMinutes();
+				int tiempo= fechaActual.getMinutes()-fechaInicial.getMinutes();
 			
 				if(tiempo>=13) {
 					lote2.setMolido(true);
